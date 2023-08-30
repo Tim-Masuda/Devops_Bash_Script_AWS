ebe10748ee75bde808260b1addd61f1b3e74320e
Tim <104795041+Tim-Masuda@users.noreply.github.com>
2023-08-30 20:50:42 +0300

    Add files via upload

---
+++ aws_resource.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+#########################
+# Author: Tim
+# Date: 30.08.23
+#
+# Version: v1
+#
+#
+# This script will report the AWS resource usage
+########################
+
+# off input
+set -x
+
+# AWS S3 
+# AWS EC2
+# AWS Lambda
+# AWS IAM user
+
+# list s3 buckets
+echo "Print s3"
+aws s3 ls > resourceTracker 
+
+# list EC2 Instance
+echo "Print EC2"
+aws ec2 describe-instances | jq '.Reservations[].Instances[].InstanceId'
+
+# list lambda
+echo "Print lambda"
+aws lambda list-functions >> resourceTracker:q!
+
+# list IAM users
+echo "Print IAM users"
+aws iam list-users
